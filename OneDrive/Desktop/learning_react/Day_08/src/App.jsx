558f6ad0284f01b1ff97a1867931daf7b4135f82
sunil dhayal <sunildhayal368@gmail.com>
2025-01-13 17:18:22 +0530

    done

--- OneDrive/Desktop/learning_react/Day_08/src/App.jsx
+++ OneDrive/Desktop/learning_react/Day_08/src/App.jsx
@@ -1,10 +1,16 @@
 
 // import './App.css'
 
+import Comment from "./Comment";
+
 function App() {
   
   return (
-       <></>
+       <>
+         
+         <Comment/>
+          
+       </>
   );
 }
 
